3e6a9a370a93982b8ab6ab9f11b8bd7c6a285ef3
atmani samira <erp.atmanis@gmail.com>
2024-05-28 20:20:33 +0200

    suite test

--- front/src/__tests__/Bills.js
+++ front/src/__tests__/Bills.js
@@ -2,54 +2,67 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
-import mockStore from "../__mocks__/store";
+import { screen, waitFor, fireEvent } from "@testing-library/dom";
+import BillsUI from "../views/BillsUI.js";
+import { bills } from "../fixtures/bills.js";
+import {  ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
 import Bills from "../containers/Bills.js";
-import router from "../app/Router.js";
+import mockStore from "../__mocks__/store";
 
+import router from "../app/Router.js";
 jest.mock("../app/Store", () => mockStore)
 
 //Étant donné que je suis connecté en tant qu'employé
 describe("Given I am connected as an employee", () => {
-   //Quand je suis sur la page Bills(factures)
+  //Quand je suis sur la page Bills(factures)
   describe("When I am on Bills Page", () => {
-      //Ensuite, l'icône de la facture dans la disposition verticale doit être mise en surbrillance
+    //Ensuite, l'icône de la facture dans la disposition verticale doit être mise en surbrillance
     test("Then bill icon in vertical layout should be highlighted", async () => {
-
+      
       // Simulation des donnée dans le locale storage
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
 
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
 
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
+      const root = document.createElement("div");
 
-      await waitFor(() => screen.getByTestId('icon-window'))
-      const windowIcon = screen.getByTestId('icon-window')
+      root.setAttribute("id", "root");
 
-      //to-do write expect expression
+      document.body.append(root);
+
+      router();
+
+      window.onNavigate(ROUTES_PATH.Bills);
+      await waitFor(() => screen.getByTestId("icon-window"));
+      const windowIcon = screen.getByTestId("icon-window");
+
       expect(windowIcon.classList.contains("active-icon")).toBe(true);
-    })
+    });
 
-      // Ensuite les notes doivent être en ordre croissant
+    // Ensuite les notes doivent être en ordre croissant
     test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
-  })
-})
+      
+      document.body.innerHTML = BillsUI({ data: bills });
+      const dates = screen
+        .getAllByText(
+          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
+        )
+        .map((a) => a.innerHTML);
+      const antiChrono = (a, b) => a - b ;
+      const datesSorted = [...dates].sort(antiChrono);
+      expect(dates).toEqual(datesSorted);
+    });
+
+  });
+});
 
 // --------------------- container/Bill------------------------
 
